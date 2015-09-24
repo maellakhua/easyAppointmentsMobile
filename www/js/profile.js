ae18afb7f343f894f682f05e7ce52367b08f3f64
Χαρλαύτης Βασίλειος <charlaftisb@gmai.com>
2015-09-24 17:30:44 +0300

    fixed profile

--- www/js/profile.js
+++ www/js/profile.js
@@ -2,38 +2,24 @@ angular.module('starter.profile', ['ionic'])
 
 .controller('ProfileCtrl', function($scope, $stateParams) {
 
-	var fname=window.localStorage['name'];
-	var surname=window.localStorage['surname'];
-	var email=window.localStorage['email'];
-	var phone=window.localStorage['phone'];
+		window.localStorage['fname']="Bill";
+		window.localStorage['surname']="Charlaftis";
+		window.localStorage['email']="it213*@hua.gr";
+		window.localStorage['phone']="1234567890";
 
-
-	if(fname){
-		$scope.fname=name;
-	}else{
-		$scope.fname="Name";
-	}if(surname){
-		$scope.surname=surname;
-	}else{
-		$scope.surname="surname";
-	}if(email){
-		$scope.email=email;
-	}else{
-		$scope.email="mail";
-	}if(phone){
-		$scope.phone=phone;
-	}else{
-		$scope.phone="phone";
-	}
+		$scope.fname=	window.localStorage['fname'];
+		$scope.surname=window.localStorage['surname'];
+		$scope.email=window.localStorage['email'];
+		$scope.phone=window.localStorage['phone'];
 
 	$scope.Submit =function (){
 
-			fname=$scope.fname;
-			surname=$scope.surname;
-			email=$scope.email;
-			phone=$scope.phone;
+			window.localStorage['fname']=$scope.fname;
+			window.localStorage['surname']=$scope.surname;
+			window.localStorage['email']=$scope.email;
+			window.localStorage['phone']=$scope.phone;
 
-			alert(fname+" "+surname+" "+email+" "+phone);
+			alert($scope.fname+window.localStorage['fname']+" "+window.localStorage['surname']+" "+window.localStorage['email']+" "+window.localStorage['phone']);
 		};
 
 }
