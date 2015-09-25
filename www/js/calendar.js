039296377cb381d53699429eda00cf8f5ac823a3
ioannis-mit <nasakas13@hotmail.com>
2015-09-25 20:17:17 +0300

    ready calendar

--- www/js/calendar.js
+++ www/js/calendar.js
@@ -5,10 +5,18 @@ angular.module('starter.calendar', ['ionic', 'ionic-datepicker','angular-datepic
 
 
 $scope.datePickerCallback = function (val) {
+    var nextPage = '';
     if (!val) {
         console.log('Date not selected',val);
+        alert("You didn't select a date");
     } else {
         console.log('Selected date is : ', val);
+        alert(val);
+        if($scope.currentDate > val){
+            alert("This date is unavailble..");
+        }else{
+            //nextPage = 'href="#/app/time"';
+        }
     }
 };
 });
