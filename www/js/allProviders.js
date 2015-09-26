a6f13b80b186f8589e5727d77a73132fcd919250
BillCharlaftis <charlaftisb@gmail.com>
2015-09-26 22:08:59 +0300

    allProviders fixed

--- www/js/allProviders.js
+++ www/js/allProviders.js
@@ -2,6 +2,9 @@ angular.module('starter.allProviders', [])
 
 .controller('AllProvidersCtrl', function($scope, $stateParams,$http) {
 
+	var prvName;
+	$scope.showme=[];
+	$scope.showme.fill(false);
 	console.log("in provider");
 	console.log($stateParams.serviceId);
 
@@ -11,9 +14,19 @@ angular.module('starter.allProviders', [])
 					headers: {'Content-type':undefined}
 			 }).success(function(data){
 				 console.log("Success",data);
-				 $scope.availbleServices = data;
+				  $scope.availbleServices= data;
 			}).error(function(){
 					console.log("Error",error);
 			});
+
+			$scope.show =function (wichHide){
+
+				$scope.showme.fill(false);
+				$scope.showme[wichHide]=true;
+				$scope.showme[prvName]=false;
+				// alert(	wichHide+" "+$scope.showme[wichHide]);
+				prvName=wichHide;
+				
+			}
 
 });
